68308f7bdc90f6250096b646184661e13f91ff76
elysium09 <eyueldaniel0921@gmail.com>
2021-07-29 10:49:53 +0300

    :hammer: Setup BottomNavigationView with navigation Component

--- app/src/main/java/com/codexo/githubclient/MainActivity.kt
+++ app/src/main/java/com/codexo/githubclient/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.codexo.githubclient
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.myFragmentContainer) as NavHostFragment?
+        val navController = navHostFragment!!.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.notificationsFragment,
+                R.id.exploreFragment,
+                R.id.profileFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        bottomNavView.setupWithNavController(navController)
     }
 }
